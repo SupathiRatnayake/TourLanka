4dacaadeaad8ef5f1cfa0e6f50d3522e5148f69f
Supathi Ratnayake <r.supathi@gmail.com>
2019-07-15 14:04:53 +0530

    tour packages

--- tlk/tourPackages.js
+++ tlk/tourPackages.js
@@ -4,7 +4,7 @@
 
 function tourObj(packName,  bCost, numOfDays, category) {
     this.packName = packName; // specefies name of tour package
-    this.bCost = bCpst;
+    this.bCost = bCost;
     this.numOfDays = numOfDays;
     this.category = category;
 }
@@ -31,3 +31,14 @@ package4.desc = `It is a long established fact that a reader will be distracted 
 package5.desc = `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`;
 
 package6.desc = `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`;
+
+packageList = [package1, package3, package4, package5, package6];
+function displayPackages() {
+    for (var i = 0; i < packageList.length; i++) {
+        var item = document.createElement('div');
+        item.innerHTML = packageList[i].packName;
+        document.getElementById('container').appendChild(item);
+    }
+}
+
+window.addEventListener('load', displayPackages);
