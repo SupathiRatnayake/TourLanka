eaa9b083b43c98e97c94d9034e0d42d084cdcfe3
Supathi Ratnayake <r.supathi@gmail.com>
2019-07-18 18:59:24 +0530

    img test

--- tlk/tourPackages.js
+++ tlk/tourPackages.js
@@ -2,21 +2,22 @@
 
 // Package constructor
 
-function tourObj(packName,  bCost, numOfDays, category) {
+function tourObj(packName,  bCost, numOfDays, category, image) {
     this.packName = packName; // specefies name of tour package
     this.bCost = bCost;
     this.numOfDays = numOfDays;
     this.category = category;
+    this.image = image;
 }
 
 // Package objects
 
-var package1 = new tourObj('package 1', 500, 5, 'budget');
-var package2 = new tourObj('package 2', 800, 5, 'budget');
-var package3 = new tourObj('package 3', 800, 5, 'budget');
-var package4 = new tourObj('package 4', 500, 5, 'budget');
-var package5 = new tourObj('package 5', 800, 5, 'budget');
-var package6 = new tourObj('package 6', 800, 5, 'budget');
+var package1 = new tourObj('package 1', 500, 5, 'budget', 'package1.jpg');
+var package2 = new tourObj('package 2', 800, 5, 'budget', 'package2.jpg');
+var package3 = new tourObj('package 3', 800, 5, 'budget', 'package3.jpg');
+var package4 = new tourObj('package 4', 500, 5, 'budget', 'package4.jpg');
+var package5 = new tourObj('package 5', 800, 5, 'budget', 'package5.jpg');
+var package6 = new tourObj('package 6', 800, 5, 'budget', 'package6.jpg');
 
 // Add a description to each package
 
@@ -35,8 +36,15 @@ package6.desc = `It is a long established fact that a reader will be distracted 
 packageList = [package1, package3, package4, package5, package6];
 function displayPackages() {
     for (var i = 0; i < packageList.length; i++) {
+        var package = packageList[i];
         var item = document.createElement('div');
-        item.innerHTML = packageList[i].packName;
+        item.setAttribute('class', 'item');
+        var title = document.createElement('h4');
+        title.innerHTML = package.packName;
+        var image = document.createElement('img');
+        image.setAttribute('src', 'images/' + package.image);
+        item.appendChild(title);
+        item.appendChild(image);
         document.getElementById('container').appendChild(item);
     }
 }
